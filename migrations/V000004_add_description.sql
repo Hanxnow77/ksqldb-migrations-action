55adce6bd0ea2652112d24a5d6e5eeaa3e3ad4de
Hanxnow77 <hansnower@gmail.com>
2022-11-17 14:25:00 +0000

    update

--- migrations/V000004_add_description.sql
+++ migrations/V000004_add_description.sql
@@ -1,2 +1,12 @@
-ALTER STREAM ALL_ORDER_EVENTS
-ADD DESCRIPTION STRING;
+CREATE STREAM NEW_ORDER_EVENTS (
+  orderid STRING,
+  name STRING,
+  items ARRAY<STRING>,
+  cost DECIMAL(6, 2),
+  ordertime TIMESTAMP,
+  purchase_method STRING
+) WITH (
+  kafka_topic='all_order_events',
+  partitions=1,
+  value_format='json'
+);
